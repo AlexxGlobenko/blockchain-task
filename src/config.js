0ee92157daabec6279d7a0091c78335c1f1c4ad3
sasha_globenko <alexandr.globenko@fusion-team.com>
2020-07-08 22:08:53 +0300

    Change server links

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ const envType = process.env.NODE_ENV || 'development';
 
 const config = {
   development: {
-    currentAddress: 'http://localhost:3000',
-    serverAddress: 'http://localhost:4000'
+    apiBaseUrl: 'https://blockchain.info',
+    secondaryApiBaseUrl: 'https://api.blockchain.info'
   },
   production: {
-    currentAddress: 'http://localhost:3000',
-    serverAddress: 'http://localhost:4000'
+    apiBaseUrl: 'https://blockchain.info',
+    secondaryApiBaseUrl: 'https://api.blockchain.info'
   }
 };
 
